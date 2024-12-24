ea43fc60a1f6ddf09f21282eb84f8de02e56804c
jin214930 <jin214930@naver.com>
2024-12-24 18:13:12 +0900

    음수가 없는 단일 연산 구현

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,5 +1,23 @@
 public class Calc {
     public static double run(String exp) {
+        exp = exp.replaceAll(" ", "");
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            switch (c) {
+                case '+':
+                    return run(exp.substring(0, i)) + run(exp.substring(i + 1));
+                case '-':
+                    if (i == 0)
+                        continue;
+                    return run(exp.substring(0, i)) - run(exp.substring(i + 1));
+                case '*':
+                    return run(exp.substring(0, i)) * run(exp.substring(i + 1));
+                case '/':
+                    return run(exp.substring(0, i)) / run(exp.substring(i + 1));
+            }
+        }
+
         return Double.parseDouble(exp);
     }
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -15,4 +15,28 @@ public class CalcTest {
     public void test2() {
         assertThat(Calc.run("-1")).isEqualTo(-1);
     }
+
+    @Test
+    @DisplayName("1 + 1 == 2")
+    public void test3() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("1 - 1 == 0")
+    public void test4() {
+        assertThat(Calc.run("1 - 1")).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("1 * 1 == 1")
+    public void test5() {
+        assertThat(Calc.run("1 * 1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("1 / 1 == 1")
+    public void test6() {
+        assertThat(Calc.run("1 / 1")).isEqualTo(1);
+    }
 }
